29e71cf41dc4da3f7c6a99c136e2b12b8ed6c6fa
pputra <mynameisyoga@live.com>
2021-03-25 11:43:37 -0700

    refactor: methods for coefficients sum and diff

--- src/DWT.java
+++ src/DWT.java
@@ -34,12 +34,7 @@ public class DWT {
             for (int j = 0; j < width; j += 2) {
                 RGB rgb1 = inputChannels[i][j];
                 RGB rgb2 = inputChannels[i][j+1];
-
-                int rAvg = (int) Math.round((rgb1.getR() + rgb2.getR()) / 2.0);
-                int gAvg = (int) Math.round((rgb1.getG() + rgb2.getG()) / 2.0);
-                int bAvg = (int) Math.round((rgb1.getB() + rgb2.getB()) / 2.0);
-
-                decompositionBuffer[i][bufferIndex] = new RGB(rAvg, gAvg, bAvg);
+                decompositionBuffer[i][bufferIndex] = getSumCoefficient(rgb1, rgb2, true);
                 bufferIndex++;
             }
         }
@@ -50,12 +45,7 @@ public class DWT {
             for (int j = 0; j < width; j += 2) {
                 RGB rgb1 = inputChannels[i][j];
                 RGB rgb2 = inputChannels[i][j+1];
-
-                int rAvg = (int) Math.round((rgb1.getR() - rgb2.getR()) / 2.0);
-                int gAvg = (int) Math.round((rgb1.getG() - rgb2.getG()) / 2.0);
-                int bAvg = (int) Math.round((rgb1.getB() - rgb2.getB()) / 2.0);
-
-                decompositionBuffer[i][bufferIndex] = new RGB(rAvg, gAvg, bAvg);
+                decompositionBuffer[i][bufferIndex] = getDiffCoefficient(rgb1, rgb2, true);
                 bufferIndex++;
             }
         }
@@ -74,13 +64,7 @@ public class DWT {
             for (int row = 0; row < height; row += 2) {
                 RGB rgb1 = inputChannels[row][col];
                 RGB rgb2 = inputChannels[row + 1][col];
-
-                int rAvg = (int) Math.round((rgb1.getR() - rgb2.getR()) / 2.0);
-                int gAvg = (int) Math.round((rgb1.getG() - rgb2.getG()) / 2.0);
-                int bAvg = (int) Math.round((rgb1.getB() - rgb2.getB()) / 2.0);
-
-                decompositionBuffer[bufferIndex][col] = new RGB(rAvg, gAvg, bAvg);
-
+                decompositionBuffer[bufferIndex][col] = getDiffCoefficient(rgb1, rgb2, true);
                 bufferIndex++;
             }
         }
@@ -91,12 +75,7 @@ public class DWT {
             for (int row = 0; row < height; row += 2) {
                 RGB rgb1 = inputChannels[row][col];
                 RGB rgb2 = inputChannels[row + 1][col];
-
-                int rAvg = (int) Math.round((rgb1.getR() + rgb2.getR()) / 2.0);
-                int gAvg = (int) Math.round((rgb1.getG() + rgb2.getG()) / 2.0);
-                int bAvg = (int) Math.round((rgb1.getB() + rgb2.getB()) / 2.0);
-
-                decompositionBuffer[bufferIndex][col] = new RGB(rAvg, gAvg, bAvg);
+                decompositionBuffer[bufferIndex][col] = getSumCoefficient(rgb1, rgb2, true);
 
                 bufferIndex++;
             }
@@ -116,25 +95,33 @@ public class DWT {
             for (int j = 0; j < midWidth; j++) {
                 RGB rgb1 = decomposedChannels[i][j];
                 RGB rgb2 = decomposedChannels[i][j+midWidth];
-
-                int constructedR = rgb1.getR() + rgb2.getR();
-                int constructedG = rgb1.getG() + rgb2.getG();
-                int constructedB = rgb1.getB() + rgb2.getB();
-
-                constructionBuffer[i][bufferIndex] = new RGB(constructedR, constructedG, constructedB);
-
-                constructedR = rgb1.getR() - rgb2.getR();
-                constructedG = rgb1.getG() - rgb2.getG();
-                constructedB = rgb1.getB() - rgb2.getB();
-
+                constructionBuffer[i][bufferIndex] = getSumCoefficient(rgb1, rgb2, false);
                 bufferIndex++;
-
-                constructionBuffer[i][bufferIndex] = new RGB(constructedR, constructedG, constructedB);
-
+                constructionBuffer[i][bufferIndex] = getDiffCoefficient(rgb1, rgb2, false);
                 bufferIndex++;
             }
         }
 
         return constructionBuffer;
+    }
+
+    private RGB getSumCoefficient(RGB rgb1, RGB rgb2, boolean isAverage) {
+        double denominator = isAverage ? 2.0 : 1.0;
+
+        int r = (int) Math.round((rgb1.getR() + rgb2.getR()) / denominator);
+        int g = (int) Math.round((rgb1.getG() + rgb2.getG()) / denominator);
+        int b = (int) Math.round((rgb1.getB() + rgb2.getB()) / denominator);
+
+        return new RGB(r, g, b);
+    }
+
+    private RGB getDiffCoefficient(RGB rgb1, RGB rgb2, boolean isAverage) {
+        double denominator = isAverage ? 2.0 : 1.0;
+
+        int r = (int) Math.round((rgb1.getR() - rgb2.getR()) / denominator);
+        int g = (int) Math.round((rgb1.getG() - rgb2.getG()) / denominator);
+        int b = (int) Math.round((rgb1.getB() - rgb2.getB()) / denominator);
+
+        return new RGB(r, g, b);
     }
 }
